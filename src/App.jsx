51f97516b5467eed7db1e42809ccd7e28afc11c0
justice jennings <justice.l.jennings@gmail.com>
2023-11-15 15:34:46 -0500

    added import/export statements to contact file and deleted useless data from app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ function App() {
   return (
     <>
       <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        
       </div>
     </>
   )
---
+++ src/components/contactList.jsx
@@ -0,0 +1,13 @@
+import App from "../App";
+import React from "react";
+
+
+
+
+
+
+
+
+
+
+export default App
